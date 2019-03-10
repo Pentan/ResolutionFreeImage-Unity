dac12121e24957f9ea03cdf1d4eb806aa542e7be
Satoru NAKAJIMA <c5h12pentan@gmail.com>
2019-03-11 03:58:15 +0900

    Add border width sliders for Fragment and Geometry shape demos

---
+++ ResolutionFreeImage/Assets/Demo/Scripts/FragCelllBorderController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using ResFreeImage.UI;
+
+namespace ResFreeImage.Demo {
+
+    public class FragCelllBorderController : MonoBehaviour
+    {
+        public Slider slider;
+
+        private FragmentShape shape;
+
+        void Start() {
+            shape = GetComponent<FragmentShape>();
+            OnSliderChanged(slider.value);
+        }
+
+        public void OnSliderChanged(float val) {
+            shape.overrideBorderWidth = val;
+            shape.SetAllDirty();
+        }
+    }
+}
---
+++ ResolutionFreeImage/Assets/Demo/Scripts/GeomCelllBorderController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using ResFreeImage.UI;
+
+namespace ResFreeImage.Demo {
+
+    public class GeomCelllBorderController : MonoBehaviour
+    {
+        public Slider slider;
+
+        private GeometryShape shape;
+
+        void Start() {
+            shape = GetComponent<GeometryShape>();
+            OnSliderChanged(slider.value);
+        }
+
+        public void OnSliderChanged(float val) {
+            shape.borderWidth = val;
+            shape.SetAllDirty();
+        }
+    }
+}
